cdeaf598387a0380b2d353f18ba9e12a8d220f72
Randall Degges <r@rdegges.com>
2014-09-29 16:38:36 -0700

    Adding a basic Express app.

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var stormpath = require('express-stormpath');
+
+var app = express();
+
+app.use(stormpath.init(app, {
+  application: process.env.STORMPATH_URL,
+}));
+
+app.get('/', function(req, res) {
+  res.send("Hey there! Thanks for visting the site! Be sure to <a href='/login'>login</a>!");
+});
+
+app.listen(process.env.PORT || 3000);
